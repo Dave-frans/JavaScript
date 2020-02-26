3e40c315491d25d22fbb57aa135ed04fc8d47ba3
Daviangelo Frans <djrfrans@st.noorderpoort.nl>
2020-02-26 09:33:33 +0100

    initial commit

---
+++ H2/Opdracht2.1.js
@@ -0,0 +1,5 @@
+
+document.getElementById("paginatitel").innerText = "Mijn geweldige pagina";
+document.getElementById("subkop").innerText = "Mijn geweldige subkop";
+document.getElementById("aap4").src = "aap7.jpg";
+document.getElementById("aap5").src = "aap7.jpg";
