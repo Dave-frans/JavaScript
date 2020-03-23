88edbacddb2350180b73a0a59abdeb9a77ca0254
Daviangelo Frans <djrfrans@st.noorderpoort.nl>
2020-03-23 12:06:31 +0100

    initial commit

--- H2/Opdracht2.2/Opdracht2.2.js
+++ H2/Opdracht2.2/Opdracht2.2.js
@@ -1,15 +1,22 @@
-var Blokje1 = {
-    kleur: "blauw"
-}
-var Blokje2 = {
-    kleur: "groen"
-}
-var Blokje3 = {
-    kleur: "geel"
-}
-var Blokje4 = {
-    kleur: "rood"
-}
+var blokje1 = document.getElementById('bl1');
+blokje1.style.backgroundColor = "cyan";
+blokje1.style.textAlign = "center";
+
+
+var blokje2 = document.getElementById('bl2');
+blokje2.style.backgroundColor = "forestgreen";
+blokje2.style.textAlign = "center";
+blokje2.style.marginTop = "1%"
+
+var blokje3 = document.getElementById('bl3');
+blokje3.style.backgroundColor = "yellow";
+blokje3.style.textAlign = "right";
+blokje3.style.marginTop = "2.5%"
+
+var blokje4 = document.getElementById('bl4');
+blokje4.style.backgroundColor = "red";
+blokje4.style.textAlign = "left";
+
 
 
 document.getElementById("bl1").innerText = "Blokje1";
